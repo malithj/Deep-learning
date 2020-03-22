fe0589947ecb2cae5f8b12b51c821fdd4f8d4bd5
Malith <malith.12@cse.mrt.ac.lk>
2020-03-21 20:27:23 -0400

    removed redundant function defs

--- HW3/text_mapping.py
+++ HW3/text_mapping.py
@@ -8,22 +8,6 @@ SOS_token = 0
 EOS_token = 1
 
 
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
-
-# Lowercase, trim, and remove non-letter characters
-
-
-def normalizeString(s):
-    s = unicodeToAscii(s.lower().strip())
-    s = re.sub(r"([.!?])", r" \1", s)
-    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
-    return s
-
-
 class Lang:
     def __init__(self, name):
         self.name = name
